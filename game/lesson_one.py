cbf5ec7a5cd5cee8d6c1c479d874060f7f39041b
项小历 <247913904@qq.com>
2019-05-07 17:18:47 +0800

    lesson_one
    习题1主要是体验python语言的输出

---
+++ game/lesson_one.py
@@ -0,0 +1,13 @@
+print("输出中文试试看看！")
+print("print out the english test")
+num=4
+print(num)
+num_float=2.5
+print(num_float)
+# 看到的结果就是：
+# 输出中文试试看看！
+# print out the english test
+# 4
+# 2.5
+# 请按任意键继续. . .
+
