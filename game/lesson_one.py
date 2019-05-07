251d043da9ed11316d0af9c83af41158c8179b0a
项小历 <247913904@qq.com>
2019-05-07 21:44:01 +0800

    Update lesson_one.py

--- game/lesson_one.py
+++ game/lesson_one.py
@@ -4,10 +4,11 @@ num=4
 print(num)
 num_float=2.5
 print(num_float)
+height=input('how height are you?\n')
 # 看到的结果就是：
 # 输出中文试试看看！
 # print out the english test
 # 4
 # 2.5
+# how height are you?
 # 请按任意键继续. . .
-
